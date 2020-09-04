ffc177b6de80cf05374ef52c9829c4e0ba299a42
LogeshwariBalaji <logeshwaribalaji3103@gmail.com>
2020-09-04 12:10:01 +0530

    Create Day 2 Assignment.py

---
+++ Day 2 Assignment.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+name ="Sai"
+name1 ="Vijay"
+name2="Yazhini"
+
+
+# In[2]:
+
+
+name2
+
+
+# In[3]:
+
+
+lst = [23,47,72,[3,4,7],26]
+
+
+# In[4]:
+
+
+lst
+
+
+# In[5]:
+
+
+lst[3][2]
+
+
+# In[7]:
+
+
+lst.append("36")
+
+
+# In[8]:
+
+
+lst
+
+
+# In[18]:
+
+
+dit={"Name":"Yazhini", "Age":"2","Mail":"yazhini.in"}
+
+
+# In[20]:
+
+
+dit
+
+
+# In[22]:
+
+
+dit.get("Name")
+
+
+# In[24]:
+
+
+dit.items()
+
+
+# In[27]:
+
+
+st={"yazhini",2,3,4,2,5,4,7,}
+
+
+# In[28]:
+
+
+st
+
+
+# In[30]:
+
+
+st1={4,5}
+
+
+# In[31]:
+
+
+st1.issubset(st)
+
+
+# In[32]:
+
+
+tup=("yazhini",96,"Vijay" )
+
+
+# In[33]:
+
+
+tup
+
+
+# In[34]:
+
+
+tup.index(96)
+
+
+# In[ ]:
+
+
+
+
